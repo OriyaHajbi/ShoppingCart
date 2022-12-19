438a459c29104c6e427c1f912eaa10a8900cc582
OriyaHajbi <64365809+OriyaHajbi@users.noreply.github.com>
2022-12-19 12:20:35 +0200

    added build to server

--- server/config/db.js
+++ server/config/db.js
@@ -2,16 +2,18 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-        console.log("MongoDB connection Success");
-    } catch (error) {
-        console.log("MongoDB connection fail");
-        process.exit(1);
-    }
+  try {
+    mongoose.set('strictQuery', false);
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connection Success');
+  } catch (error) {
+    console.log("🚀 ~ file: db.js:13 ~ connectDB ~ error", error)
+    console.log('MongoDB connection fail');
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
